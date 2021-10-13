bc0d91b8cb8ff7fbd79b1e786b1a8bf489272df9
mcgloch <leanne_mcl@hotmail.com>
2021-10-13 16:30:19 +0100

    custom link and hero added
    comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import Link from '@/components/Link'
+import Link from '@/components/CustomLink'
 import { PageSEO } from '@/components/SEO'
+import Hero from "@/components/Hero";
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
@@ -20,10 +21,11 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <Hero />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            Latest blog posts
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
