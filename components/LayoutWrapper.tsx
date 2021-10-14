3a7d0821046c0d8dccd580dc708972c6103f8aed
mcgloch <leanne_mcl@hotmail.com>
2021-10-14 12:45:49 +0100

    Updated Header and Hero
    Header component now has background blur and close icon animation. Hero animation bugs fixed.

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,59 +1,20 @@
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
-import Link from './Link'
-import SectionContainer from './SectionContainer'
-import Footer from './Footer'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
-import { ReactNode } from 'react'
+import SectionContainer from "./SectionContainer";
+import Footer from "./Footer";
+import Header from "./Header";
+import { Container } from "@/lib/types/common";
 
-interface Props {
-  children: ReactNode
-}
-
-const LayoutWrapper = ({ children }: Props) => {
+const LayoutWrapper = ({ children }: Container) => {
   return (
-    <SectionContainer>
-      <div className="flex flex-col justify-between h-screen">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
-        <main className="mb-auto">{children}</main>
+    <>
+      <Header />
+      <SectionContainer>
+        <div className="flex flex-col justify-between min-h-screen">
+          <main>{children}</main>
+        </div>
         <Footer />
-      </div>
-    </SectionContainer>
-  )
-}
+      </SectionContainer>
+    </>
+  );
+};
 
-export default LayoutWrapper
+export default LayoutWrapper;
